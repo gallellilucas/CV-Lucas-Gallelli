849f8d453acdcf4a15d528ed42872a411622ac4e
Lucas Gallelli <lucasegallelli@icloud.com>
2023-01-13 12:25:08 -0300

    Editando

--- LEGCV.js
+++ LEGCV.js
@@ -1,7 +1,12 @@
+//Funcion de ventanas modales/
 let modal = document.getElementById('ventana-modal');
+let modal2 = document.getElementById('ventana-modal-2');
 let flex = document.getElementById('flex');
+let flex2 = document.getElementById('flex-2');
 let abrir = document.getElementById('abrir');
+let abrir2 = document.getElementById('abrir-2');
 let cerrar = document.getElementById('close');
+let cerrar2 = document.getElementById('close-2');
 
 abrir.addEventListener('click', function(){
     modal.style.display = 'block';
@@ -18,10 +23,6 @@ window.addEventListener('click', function(e){
     }
 });
 
-let modal2 = document.getElementById('ventana-modal-2');
-let flex2 = document.getElementById('flex-2');
-let abrir2 = document.getElementById('abrir-2');
-let cerrar2 = document.getElementById('close-2');
 
 abrir2.addEventListener('click', function(){
     modal2.style.display = 'block';
@@ -37,6 +38,3 @@ window.addEventListener('click', function(e){
         modal2.style.display = 'none';
     }
 });
-
-
-
