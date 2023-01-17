248517b63c504ba5a2c119916b4a862ccc160bfa
Lucas Gallelli <lucasegallelli@icloud.com>
2023-01-17 15:24:58 -0300

    Seguimos editando

--- LEGCV.js
+++ LEGCV.js
@@ -1,3 +1,11 @@
+//Loading page/
+
+const loader= this.document.querySelector ('.loader')
+
+window.addEventListener("load", function (){
+loader.className += " hidden";
+});
+
 //Funcion de ventanas modales/
 let modal = document.getElementById('ventana-modal');
 let modal2 = document.getElementById('ventana-modal-2');
@@ -8,33 +16,28 @@ let abrir2 = document.getElementById('abrir-2');
 let cerrar = document.getElementById('close');
 let cerrar2 = document.getElementById('close-2');
 
-abrir.addEventListener('click', function(){
+abrir.addEventListener('click', function(e){
     modal.style.display = 'block';
 });
-
-cerrar.addEventListener('click', function(){
+cerrar.addEventListener('click', function(e){
     modal.style.display = 'none';
 });
-
 window.addEventListener('click', function(e){
     console.log(e.target);
     if(e.target == flex){
         modal.style.display = 'none';
     }
 });
-
-
-abrir2.addEventListener('click', function(){
+abrir2.addEventListener('click', function(e){
     modal2.style.display = 'block';
 });
-
-cerrar2.addEventListener('click', function(){
+cerrar2.addEventListener('click', function(e){
     modal2.style.display = 'none';
 });
-
 window.addEventListener('click', function(e){
     console.log(e.target);
     if(e.target == flex2){
         modal2.style.display = 'none';
     }
 });
+
