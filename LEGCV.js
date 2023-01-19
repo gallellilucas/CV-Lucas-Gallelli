6a2b0d3a80cf50cf3733dd4bda07bc62acfa2244
Lucas Gallelli <lucasegallelli@icloud.com>
2023-01-19 14:20:21 -0300

    19/1/2023

--- LEGCV.js
+++ LEGCV.js
@@ -42,3 +42,23 @@ window.addEventListener('click', function(e){
     }
 });
 
+//NavBar //
+const sections = document.querySelectorAll ("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop -60) {
+        current = section.getAttribute ("id");}
+});
+
+    navLi.forEach ((li) => {
+        li.classList.remove("active")
+       if(li.href.includes(current)) {
+            li.classList.add("active");
+        }
+    });
+};
---
+++ Main.js
@@ -0,0 +1,54 @@
+//Loading page/
+
+const loader= this.document.querySelector ('.loader')
+
+window.addEventListener("load", function (){
+loader.className += " hidden";
+});
+
+//Funcion de ventanas modales/
+
+let modal = document.getElementById('ventana-modal');
+let modal2 = document.getElementById('ventana-modal-2');
+
+let abrir = document.getElementById('abrir');
+let abrir2 = document.getElementById('abrir-2');
+
+let cerrar = document.getElementById('close');
+let cerrar2 = document.getElementById('close-2');
+
+
+abrir.addEventListener('click', function(e){
+    modal.style.display = 'block';
+});
+cerrar.addEventListener('click', function(e){
+    modal.style.display = 'none';
+});
+
+abrir2.addEventListener('click', function(e){
+    modal2.style.display = 'block';
+});
+
+cerrar2.addEventListener('click', function(e){
+    modal2.style.display = 'none';
+});
+
+// Boton ver más //
+
+const ver = this.document.getElementById("ver");
+const ver2 = this.document.getElementById("ver-2");
+const p1 = this.document.querySelector('.p1');
+const p2 = this.document.querySelector('.p2');
+
+ver.addEventListener("click", function(){
+    p1.style.display += "block";
+    ver.className += " ocultar";
+});
+
+ver2.addEventListener("click", function(){
+    p2.style.display += "block";
+    ver2.className += " ocultar";
+});
+
+
+
